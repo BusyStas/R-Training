f830d4476139f5adefffa84697549a97048a3e8a
STANISLAV PRIKHODKO <BusyStas@users.noreply.github.com>
2015-04-25 22:07:12 -0400

    Update Plot1.R

--- Pollution/Plot1.R
+++ Pollution/Plot1.R
@@ -5,3 +5,17 @@
 ## of the years 1999, 2002, 2005, and 2008.
 ## Upload a PNG file containing your plot addressing this question.
 
+d <- readRDS("summarySCC_PM25.rds")
+View(d[1:20,])
+
+library(plyr)
+y <- ddply(d,~year,summarise,t=format(sum(Emissions),scientific=FALSE))
+#y <- format(y, scientific=FALSE)
+
+png(filename="Plot1.png", width=480, height=480, units="px")
+plot(y, type="l", xlab="Year", 
+     main = "Total Emissions in the United States from 1999 to 2008", 
+     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
+dev.off()
+
+
