ed8ab2ce1cc63da40a6188b39b225dc1b25fbac9
BusyStas <bizzik@gmail.com>
2015-04-26 14:56:18 -0400

    los angeles

--- Pollution/Plot6.R
+++ Pollution/Plot6.R
@@ -1,6 +1,8 @@
 # Plot6.R
-## How have emissions from motor vehicle sources
-## changed from 1999-2008 in Baltimore City?
+## Compare emissions from motor vehicle sources in Baltimore City
+## with emissions from motor vehicle sources in Los Angeles County
+## , California (fips == "06037"). Which city has seen
+## greater changes over time in motor vehicle emissions?
 setwd("c:/users/stas/OneDrive/Code Projects/git/R-Training/Pollution")
 
 library(plyr) 
@@ -11,10 +13,15 @@ vehicle <- cc[grepl(cc$Short.Name,pattern='vehicle',ignore.case=TRUE),]
 
 options(scipen=999) ## disabling scientific notation in R
 vehicle_in_baltimore <- d[d$SCC %in% vehicle$SCC & d$fips=="24510",] 
-y <- ddply(vehicle_in_baltimore,~year,summarise,t=sum(Emissions))
+vehicle_in_los_angeles <- d[d$SCC %in% vehicle$SCC & d$fips=="06037",] 
+y_baltimore <- ddply(vehicle_in_baltimore,~year,summarise,t=sum(Emissions))
+y_los_angeles <- ddply(vehicle_in_los_angeles,~year,summarise,t=sum(Emissions))
 
 png(filename="Plot6.png", width=480, height=480, units="px")
-plot(y, type="l", xlab="Year", 
+plot(y_baltimore, type="l", xlab="Year", 
+     main = "Total Emissions from Vehicle related sourses \nin Baltimore, MD from 1999 to 2008", 
+     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
+plot(y_los_angeles, type="l", xlab="Year", 
      main = "Total Emissions from Vehicle related sourses \nin Baltimore, MD from 1999 to 2008", 
      ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
 dev.off()
