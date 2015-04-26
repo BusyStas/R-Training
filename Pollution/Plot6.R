ea0d2f883b599e268bd4caeb642000cd5cdd9b15
BusyStas <bizzik@gmail.com>
2015-04-26 15:05:01 -0400

    los angeles using ggplot

--- Pollution/Plot6.R
+++ Pollution/Plot6.R
@@ -5,23 +5,24 @@
 ## greater changes over time in motor vehicle emissions?
 setwd("c:/users/stas/OneDrive/Code Projects/git/R-Training/Pollution")
 
-library(plyr) 
+library(plyr)
+library(ggplot2)
 d <- readRDS("summarySCC_PM25.rds")
 cc <- readRDS("Source_Classification_Code.rds")
 vehicle <- cc[grepl(cc$Short.Name,pattern='vehicle',ignore.case=TRUE),]
 ## View(vehicle)  
 
 options(scipen=999) ## disabling scientific notation in R
-vehicle_in_baltimore <- d[d$SCC %in% vehicle$SCC & d$fips=="24510",] 
-vehicle_in_los_angeles <- d[d$SCC %in% vehicle$SCC & d$fips=="06037",] 
-y_baltimore <- ddply(vehicle_in_baltimore,~year,summarise,t=sum(Emissions))
-y_los_angeles <- ddply(vehicle_in_los_angeles,~year,summarise,t=sum(Emissions))
+MD_vs_CA <- d[d$SCC %in% vehicle$SCC & (d$fips=="24510" | d$fips=="06037"),] 
+y <- ddply(MD_vs_CA,~year,summarise,t=sum(Emissions))
 
 png(filename="Plot6.png", width=480, height=480, units="px")
-plot(y_baltimore, type="l", xlab="Year", 
-     main = "Total Emissions from Vehicle related sourses \nin Baltimore, MD from 1999 to 2008", 
-     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
-plot(y_los_angeles, type="l", xlab="Year", 
-     main = "Total Emissions from Vehicle related sourses \nin Baltimore, MD from 1999 to 2008", 
-     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
+ggplot(MD_vs_CA, aes(year, Emissions, color = fips))+ 
+  geom_line(stat = "summary", fun.y = "sum")+
+  ylab(expression(paste("Total ", PM[2.5], " Emissions")))+
+  ggtitle("Total Emissions in Baltimore, MD from 1999 to 2008")+
+  scale_colour_discrete(name = "Group")
+    , label = c("Los Angeles","Baltimore"))
 dev.off()
+
+
