0a097c70c5f00da7710df878c668a1ead17d3776
BusyStas <bizzik@gmail.com>
2015-04-26 15:27:58 -0400

    baltimoere is winner

--- Pollution/Plot6.R
+++ Pollution/Plot6.R
@@ -5,24 +5,24 @@
 ## greater changes over time in motor vehicle emissions?
 setwd("c:/users/stas/OneDrive/Code Projects/git/R-Training/Pollution")
 
-library(plyr)
-library(ggplot2)
+library(plyr) 
 d <- readRDS("summarySCC_PM25.rds")
 cc <- readRDS("Source_Classification_Code.rds")
 vehicle <- cc[grepl(cc$Short.Name,pattern='vehicle',ignore.case=TRUE),]
 ## View(vehicle)  
 
 options(scipen=999) ## disabling scientific notation in R
-MD_vs_CA <- d[d$SCC %in% vehicle$SCC & (d$fips=="24510" | d$fips=="06037"),] 
-y <- ddply(MD_vs_CA,~year,summarise,t=sum(Emissions))
+vehicle_in_baltimore <- d[d$SCC %in% vehicle$SCC & d$fips=="24510",] 
+vehicle_in_los_angeles <- d[d$SCC %in% vehicle$SCC & d$fips=="06037",] 
+y_baltimore <- ddply(vehicle_in_baltimore,~year,summarise,t=sum(Emissions))
+y_los_angeles <- ddply(vehicle_in_los_angeles,~year,summarise,t=sum(Emissions))
 
 png(filename="Plot6.png", width=480, height=480, units="px")
-ggplot(MD_vs_CA, aes(year, Emissions, color = fips))+ 
-  geom_line(stat = "summary", fun.y = "sum")+
-  ylab(expression(paste("Total ", PM[2.5], " Emissions")))+
-  ggtitle("Total Emissions in Baltimore, MD from 1999 to 2008")+
-  scale_colour_discrete(name = "Group")
-    , label = c("Los Angeles","Baltimore"))
+par(mfcol=c(1,2)) # two plots side by side
+plot(y_baltimore, type="l", xlab="Year", 
+     main = "Baltimore, MD",
+     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
+plot(y_los_angeles, type="l", xlab="Year", 
+     main = "Los Angeles, CA", 
+     ylab = expression(paste("Total ", PM[2.5], " Emissions"))   ) 
 dev.off()
-
-
