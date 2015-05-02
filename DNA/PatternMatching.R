7cddde2335fd979d2e6f64cac7ebb650448a8118
BusyStas <bizzik@gmail.com>
2015-05-02 00:19:21 -0400

    dna kmer pattern matching

--- DNA/PatternMatching.R
+++ DNA/PatternMatching.R
@@ -1,14 +1,15 @@
 KmerMatch <- function (str, kmer) {
   k <- nchar(kmer)
+  idxs <- ""
   idx <- rep(0,0) # empty array 
   for (i in k+1:nchar(str)-k) {
     t <- substr(str, i, i+k-1) 
-    if (nchar(t)<k) return (idx) 
+    if (nchar(t)<k) return (idxs) 
     if (t==kmer) {
       idx[length(idx)+1] <- i-1
-      
+      idxs <- paste(idxs, i)
     }
   }
-  idx
+  idxs
 }
 
