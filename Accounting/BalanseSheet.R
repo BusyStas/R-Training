3204faa05a948e53252a1bdd0eb7369a12c5b204
BusyStas <bizzik@gmail.com>
2015-05-31 17:28:12 -0400

    balance sheet equation

---
+++ Accounting/BalanseSheet.R
@@ -0,0 +1,19 @@
+ 
+#Ending Retained Earnings = Beginning Retained Earnings + Net Income - Dividends
+#=> 60 = 50 + NI - 15 => NI = 25;
+#Net Income = Revenues - Expenses
+#=> Rev - 100 = 25
+#=> Rev = 125
+
+TotalAssets <- 300
+TotalLiabilities <- 120
+TotalEquity <- NA
+BeginningRetainedEarnings <- 30
+EndingRetainedEarnings <- NA
+Dividends <- 10
+Revenues <- 190
+Expenses <- 140
+NetIncome <- (Revenues - Expenses)
+Cash <- 50
+EndingRetainedEarnings <- BeginningRetainedEarnings+NetIncome-Dividends
+
