b47e4061f356e96babcebaaa99b0e5d8012c94bd
BusyStas <bizzik@gmail.com>
2015-06-05 17:01:02 -0400

    calculus taylor series

---
+++ Calculus/Chapter1_Functions.R
@@ -0,0 +1,16 @@
+f<-function(x){ return (atan(x)/(x^3+7*x))} 
+f<-function(x){ return ((cos(3*x)-cos(5*x))/x^2)} 
+
+x<-seq(0,.001,0.0001)
+f(x)
+1/0.1428571
+
+n <- 0:15
+sum((sqrt(2)*pi)^n/factorial(n))
+1+pi*log(sqrt(2))
+1/(1-pi*sqrt(2))
+exp(sqrt(2)*pi)
+pi*exp(sqrt(2))
+atan(sqrt(2)*pi)
+sqrt(2)/(1-pi)
+exp(sqrt(2*pi))
